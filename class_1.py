e66683522c91078f1fb9f8b899ebeb106781e952
Student <Student@Train11-1-PC325.control.ad.sit.kmutt.ac.th>
2025-07-22 16:26:08 +0700

    sunkwan

---
+++ class_1.py
@@ -0,0 +1,30 @@
+# x = int(input("number x "))
+# y = int(input("number y "))
+
+# if x > y :
+#     print("x > y")
+# elif x == y :
+#     print("x = y")
+# elif x < y :
+#     print("x < y")
+# else :
+#     print("")
+
+
+x = int(input("number x "))
+y = int(input("number y "))
+
+if x > y :
+    print("x > y")
+    XY1 = x*y
+    print(XY1)
+elif x == y :
+    print("x = y")
+    XY2 = x/y
+    print(XY2)
+elif x < y :
+    print("x < y")
+    XY3 = x+y
+    print(XY3)
+else :
+    print("I don't know TT")
---
+++ class_2.py
@@ -0,0 +1,30 @@
+distance = int(input("Enter distance : "))
+vat = 0.07
+
+if distance < 5 :
+    print("FREE!!!")
+    vatprice = 0 * vat 
+    finalprice = 0 + vatprice
+elif distance >= 5 and distance <= 50 :
+    print("10 BATH")
+    vatprice = 10 * vat 
+    finalprice = 10 + vatprice
+elif distance > 50 and distance <= 100 :
+    print("15 BATH")
+    vatprice = 15 * vat 
+    finalprice = 15 + vatprice
+elif distance > 100 and distance <= 300 :
+    print("25 BATH")
+    vatprice = 25 * vat 
+    finalprice = 25 + vatprice
+elif distance > 300 and distance <= 500 :
+    print("35 BATH")
+    vatprice = 35 * vat 
+    finalprice = 35 + vatprice
+else :
+    print("45 BATH")
+    vatprice = 45 * vat 
+    finalprice = 45 + vatprice
+
+print("Vat : " ,vatprice)
+print(finalprice, "BATH")
