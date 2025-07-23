1836a7fcbb8794bc753ff05b04fd4722a0b782ac
Student <Student@Train11-1-PC325.control.ad.sit.kmutt.ac.th>
2025-07-23 14:10:47 +0700

    pythongame

--- class_6.py
+++ class_6.py
@@ -15,8 +15,17 @@ if menu == 1 :
     print("Name :",monstername,"Hp :",monsterhp)
     round = int(input("Enter number of round U want to play : "))
     for i in range (round) :
+        if monsterhp == 0 :
+            print("You win !!!")
+            break
+        elif monsterhp <= 0 :
+                monsterhp = 300
+                print("Nigaboss is reincarnated !!!")
+                print("Name :",monstername," Hp :",monsterhp)
         weapon = str(input("Choose your weapon Bow,Staff,Sword : "))
-        if roundplus != round :
+        if roundplus == round :
+            print("You died because the round is over")
+        elif roundplus != round :
             if weapon == ("Sword") and monsterhp > 0:
                 monsterhp = monsterhp-sworddm
                 print("Name :",monstername," Hp :",monsterhp)
@@ -29,12 +38,6 @@ if menu == 1 :
                 monsterhp = monsterhp-staffdm
                 print("Name :",monstername," Hp :",monsterhp)
                 roundplus = roundplus + 1
-            elif monsterhp < 0 :
-                monsterhp = 300
-                print("Nigaboss is reincarnated !!!")
-                print("Name :",monstername," Hp :",monsterhp)
-        else :
-            print("You died because the round is over")
 
 elif menu == 2 :
     print("You died because Nigaboss is too fast.")
